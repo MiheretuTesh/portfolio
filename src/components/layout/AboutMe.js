1d818a5adfd77154c2e4beebe1decd703509a087
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-10-29 16:40:40 +0300

    fix: sty;e error fixed

--- src/components/layout/AboutMe.js
+++ src/components/layout/AboutMe.js
@@ -1,73 +1,77 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./AboutMe.css";
+import HeroText from "./HeroText";
 
 function AboutMe() {
   return (
-    <body className="aboutme">
-      <div class="container-aboutme">
-        <div class="card-aboutme">
-          <div class="face face1">
-            <div class="content">
-              <img
-                src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true"
-                alt=""
-              />
-              <h3>Design</h3>
+    <div className="aboutme-section pt-5 pb-5">
+      <h1>I am a ...</h1>
+      <div className="aboutme">
+        <div class="container-aboutme">
+          <div class="card-aboutme">
+            <div class="face face1">
+              <div class="content">
+                <img
+                  src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true"
+                  alt=""
+                />
+                <h3>Design</h3>
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+                  cum cumque minus iste veritatis provident at.
+                </p>
+                <Link to="">Read More</Link>
+              </div>
             </div>
           </div>
-          <div class="face face2">
-            <div class="content">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-                cum cumque minus iste veritatis provident at.
-              </p>
-              <Link to="">Read More</Link>
+          <div class="card-aboutme">
+            <div class="face face1">
+              <div class="content">
+                <img
+                  src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true"
+                  alt=""
+                />
+                <h3>Code</h3>
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+                  cum cumque minus iste veritatis provident at.
+                </p>
+                <Link to="">Read More</Link>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="card-aboutme">
-          <div class="face face1">
-            <div class="content">
-              <img
-                src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true"
-                alt=""
-              />
-              <h3>Code</h3>
+          <div class="card-aboutme">
+            <div class="face face1">
+              <div class="content">
+                <img
+                  src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true"
+                  alt=""
+                />
+                <h3>Launch</h3>
+              </div>
             </div>
-          </div>
-          <div class="face face2">
-            <div class="content">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-                cum cumque minus iste veritatis provident at.
-              </p>
-              <Link to="">Read More</Link>
-            </div>
-          </div>
-        </div>
-        <div class="card-aboutme">
-          <div class="face face1">
-            <div class="content">
-              <img
-                src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/launch_128.png?raw=true"
-                alt=""
-              />
-              <h3>Launch</h3>
-            </div>
-          </div>
-          <div class="face face2">
-            <div class="content">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-                cum cumque minus iste veritatis provident at.
-              </p>
-              <Link to="">Read More</Link>
+            <div class="face face2">
+              <div class="content">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+                  cum cumque minus iste veritatis provident at.
+                </p>
+                <Link to="">Read More</Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </body>
+    </div>
   );
 }
 
--- src/components/layout/HeroText.js
+++ src/components/layout/HeroText.js
@@ -1,21 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const HeroText = ({ text, color, fontsize, fontbold, width }) => {
+const HeroText = ({ text, color, fontsize, fontbold, width, padding }) => {
   const style = {
     color: `${color}`,
     fontSize: fontsize,
-    fontbold: fontbold,
+    fontBold: fontbold,
     width: width,
+    padding: padding,
   };
   return <h1 style={style}>{text}</h1>;
 };
 
 HeroText.propTypes = {
   text: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
   fontsize: PropTypes.number.isRequired,
   fontbold: PropTypes.number.isRequired,
   width: PropTypes.number.isRequired,
+  padding: PropTypes.number.isRequired,
 };
 
 export default HeroText;

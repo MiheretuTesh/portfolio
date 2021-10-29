335c760387e30b9b444a70975f21769587a785b1
Miheretu Teshale <se.miheretu.degebassa@gmail.com>
2021-10-29 09:58:48 +0300

    feat: Navbar is set to App file.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
-import React from 'react';
-
+import "./App.css";
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <>
-      
+      <Switch>
+        <Navbar />
+        {/* <Route exact path="/" component={Home} /> */}
+        {/* <Route exact path="/page" component={HashLinkPage} /> */}
+        {/* <Route render={() => <Redirect to="/" />} /> */}
+      </Switch>
     </>
   );
 }
